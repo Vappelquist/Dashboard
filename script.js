7c28e0fbd7b8e9bbc6d4d71812060ab3bf05378b
Vilgot <vappelquist03@gmail.com>
2026-03-28 16:54:25 +0100

    Enhance layout and functionality: add responsive design, background change feature, and link addition

--- script.js
+++ script.js
@@ -23,3 +23,20 @@ updateClock();
 updateDate();
 setInterval(updateClock, 6000);
 setInterval(updateDate, 1000);
+
+async function bgchange(){
+
+  const baseURL = "https://api.unsplash.com/photos/random?client_id="
+  const key = "KcKYMJ7HVXlAZ-FUNTklxu4IZk5iwjJOua0-PRxqJKE"
+  
+  const res = await fetch(`${baseURL}${key}`)
+  const bg  =await res.json()
+  console.log(bg);
+document.body.style.backgroundImage = `url(${bg.urls.regular})`
+  console.log({baseURL},{key})
+  console.log(bg)
+
+  if (!res.ok){
+    console.log("Något gick fel")
+  }
+}
